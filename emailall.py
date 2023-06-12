5d3ec88fb9a3919eb62454824192a6c2b0cf4ae9
Shun Lyu <shun.lyu@oneasg.com>
2023-06-12 21:36:41 +1200

    Changing interface, Allow create instance and run multiple domains

--- emailall.py
+++ emailall.py
@@ -5,21 +5,19 @@ from common.output import Output
 class EmailAll:
     """
     EmailAll is a powerful Email Collect tool
-
-    Example:
-        python3 emailall.py --domain example.com run
     """
 
-    def __init__(self, domain=None):
-        self.domain = domain.lower().strip()  # 单条
-        self.output = Output()  # 输出结果类
+    def __init__(self):
+        self.domain = None
+        self.output = Output()
 
-    def run(self):
+    def run(self, domain):
+        self.domain = domain.lower().strip()
         collect = Collect(self.domain)
         result = collect.run()
         return result
 
 
 if __name__ == "__main__":
-    emailAll = EmailAll("test.com")
-    emailAll.run()
+    emailAll = EmailAll()
+    result = emailAll.run("test.com")
